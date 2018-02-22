dad1e4afcc7ecb27408cb422ac2b693f085d5f02
musialm <m.musial@stat.gov.pl>
2018-02-22 16:01:17 +0100

    Zydzik_OGL: 1_5.  5. Migracje: Metoda Seed()

--- Repozytorium/Migrations/Configuration.cs
+++ Repozytorium/Migrations/Configuration.cs
@@ -4,6 +4,9 @@ namespace Repozytorium.Migrations
   using System.Data.Entity;
   using System.Data.Entity.Migrations;
   using System.Linq;
+  using Microsoft.AspNet.Identity;
+  using Microsoft.AspNet.Identity.EntityFramework;
+  using Repozytorium.Models;
 
   internal sealed class Configuration : DbMigrationsConfiguration<Repozytorium.Models.OglContext>
   {
@@ -15,10 +18,121 @@ namespace Repozytorium.Migrations
 
     protected override void Seed(Repozytorium.Models.OglContext context)
     {
-      //  This method will be called after migrating to the latest version.
+      // Do debugowania metody seed
+      // if (System.Diagnostics.Debugger.IsAttached == false)
+      //    System.Diagnostics.Debugger.Launch();
+      SeedRoles(context);
+      SeedUsers(context);
+      SeedOgloszenia(context);
+      SeedKategorie(context);
+      SeedOgloszenie_Kategoria(context);
 
-      //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-      //  to avoid creating duplicate seed data.
+    }
+
+
+    private void SeedRoles(OglContext context)
+    {
+      var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>());
+
+      if (!roleManager.RoleExists("Admin"))
+      {
+        var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+        role.Name = "Admin";
+        roleManager.Create(role);
+      }
+      if (!roleManager.RoleExists("Pracownik"))
+      {
+        var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+        role.Name = "Pracownik";
+        roleManager.Create(role);
+      }
+
+    }
+
+    private void SeedUsers(OglContext context)
+    {
+      var store = new UserStore<Uzytkownik>(context);
+      var manager = new UserManager<Uzytkownik>(store);
+      if (!context.Users.Any(u => u.UserName == "Admin"))
+      {
+
+        var user = new Uzytkownik { UserName = "Admin"};
+
+        var adminresult = manager.Create(user, "12345678");
+
+        if (adminresult.Succeeded)
+          manager.AddToRole(user.Id, "Admin");
+      }
+
+      if (!context.Users.Any(u => u.UserName == "Marek"))
+      {
+        var user = new Uzytkownik { UserName = "marek@AspNetMvc.pl" };
+        var adminresult = manager.Create(user, "1234Abc,");
+        if (adminresult.Succeeded)
+          manager.AddToRole(user.Id, "Pracownik");
+      }
+      if (!context.Users.Any(u => u.UserName == "Prezes"))
+      {
+        var user = new Uzytkownik { UserName = "prezes@AspNetMvc.pl" };
+        var adminresult = manager.Create(user, "1234Abc,");
+        if (adminresult.Succeeded)
+          manager.AddToRole(user.Id, "Admin");
+      }
+
+    }
+
+    private void SeedOgloszenia(OglContext context)
+    {
+
+      var idUzytkownika = context.Set<Uzytkownik>().Where(u => u.UserName == "Admin").FirstOrDefault().Id;
+      for (int i = 1; i <= 10; i++)
+      {
+        var ogl = new Ogloszenie()
+        {
+          Id = i,
+          UzytkownikId = idUzytkownika,
+          Tresc = "Treœæ og³oszenia" + i.ToString(),
+          Tytul = "Tytu³ og³oszenia" + i.ToString(),
+          DataDodania = DateTime.Now.AddDays(-i)
+        };
+        context.Set<Ogloszenie>().AddOrUpdate(ogl);
+      }
+      context.SaveChanges();
+    }
+
+    private void SeedKategorie(OglContext context)
+    {
+      for (int i = 1; i <= 10; i++)
+      {
+        var kat = new Kategoria()
+        {
+          Id = i,
+          Nazwa = "Nazwa kategorii" + i.ToString(),
+          Tresc = "Treœæ og³oszenia" + i.ToString(),
+          MetaTytul = "Tytu³ kategorii" + i.ToString(),
+          MetaOpis = "Opis kategorii" + i.ToString(),
+          MetaSlowa = "S³owa kluczowe do kategorii" + i.ToString(),
+          ParentId = i
+        };
+        context.Set<Kategoria>().AddOrUpdate(kat);
+      }
+      context.SaveChanges();
+    }
+
+    private void SeedOgloszenie_Kategoria(OglContext context)
+    {
+      for (int i = 1; i < 10; i++)
+      {
+        var okat = new Ogloszenie_Kategoria()
+        {
+          Id = i,
+          OgloszenieId = i / 2 + 1,
+          KategoriaId = i / 2 + 2
+        };
+
+        context.Set<Ogloszenie_Kategoria>().AddOrUpdate(okat);
+      }
+      context.SaveChanges();
     }
   }
 }
