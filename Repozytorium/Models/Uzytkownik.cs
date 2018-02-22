742cfb6d0e2b09240152ebed4b9614b9cabfae26
musialm <m.musial@stat.gov.pl>
2018-02-22 15:13:09 +0100

    Zydzik_OGL: 1_3.  OglContext:  Tworzenie klasy kontekstu

---
+++ Repozytorium/Models/Uzytkownik.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Repozytorium.Models 
+{
+  // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+  public class Uzytkownik : IdentityUser
+  {
+    public Uzytkownik() { 
+      this.Ogloszenia = new HashSet<Ogloszenie>(); 
+    }    
+    public string Imie { get; set; }
+
+    public string Nazwisko { get; set; }
+
+    #region dodatkowe pole notmapped    
+    [NotMapped]     
+    [Display(Name = "Pan/Pani:")]    
+    public string PelneNazwisko    {        get { return Imie + " " + Nazwisko; }    }
+    #endregion
+
+    public virtual ICollection<Ogloszenie> Ogloszenia { get; private set; }
+
+    public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<Uzytkownik> manager)
+    {
+      // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+      var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+      // Add custom user claims here
+      return userIdentity;
+    }
+  }
+}
