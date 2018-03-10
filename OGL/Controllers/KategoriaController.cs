5a0c591e11feb007b820847ea51771bc2be62536
musialm <m.musial@stat.gov.pl>
2018-03-10 22:07:12 +0100

    Zydzik_OGL_Dod: 1: Wyświetlanie danych z API na stronie przy pomocy jQuery

--- OGL/Controllers/KategoriaController.cs
+++ OGL/Controllers/KategoriaController.cs
@@ -39,11 +39,18 @@ namespace OGL.Controllers
       return View(model);
     }
 
+
     [Route("JSON")]
     public ActionResult KategorieWJson()
     {
       var kategorie = _repo.PobierzKategorie().AsNoTracking();
       return Json(kategorie, JsonRequestBehavior.AllowGet);
+    }
+
+
+    public ActionResult Lista()
+    {
+      return View();
     }
 
   }
