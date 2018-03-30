8c5e5eee209b9fb91467dfdba774eb900a566eac
musialm <m.musial@stat.gov.pl>
2018-03-30 11:05:41 +0200

    Zydzik_OGL_Dod: 2: Przykład zapisywania zdjęć do Sql Servera: http://www.advancesharp.com/blog/1142/asp-net-mvc-upload-file-with-record-validation-step-6

---
+++ Repozytorium/Models/Produkty.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Repozytorium.Models
+{
+  public class Produkty
+  {
+    [Key, Display(Name = "Id produktu:")]
+    public Int32 ProductId { get; set; }
+
+    [Required]
+    [Display(Name = "Nazwa produktu")]
+    public String ProductName { get; set; }
+
+    [Required]
+    [Display(Name = "Price")]
+    public Decimal Price { get; set; }
+
+    [Required]
+    [Display(Name = "Category")]
+    public Int32 CategoryId { get; set; }
+
+    [Required]
+    [DataType(DataType.Upload)]
+    [Display(Name = "Wybierz plik")]
+    public HttpPostedFileBase ImageUpload { get; set; }
+  }
+}
